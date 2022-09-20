9e1437d0ae4b7a4f556e6edd288172432326bb97
gm sadiq <gm1sadiq@gmail.com>
2022-09-20 21:52:06 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config()
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('welcome healing hospital!')
+})
+
+app.listen(port, () => {
+  console.log(`healing hospital listening on port ${port}`)
+})
