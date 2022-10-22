94ef0f678b7e65d030918677dad372f8fc53d252
gm sadiq <gm1sadiq@gmail.com>
2022-10-22 22:26:40 +0600

    booking post

--- index.js
+++ index.js
@@ -8,31 +8,43 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.jbgbo.mongodb.net/?retryWrites=true&w=majority"`
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.jbgbo.mongodb.net/?retryWrites=true&w=majority`
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 async function run() {
   try {
-    await client.connect();
-      const database = client.db("healling_hospital");
-      const serviceCollection = database.collection("services");
+    await client.connect()
+    const serviceCollection = client.db("healling_hospital").collection("services");
+    const bookingCollection = client.db("healling_hospital").collection("bookings")
 
+     // create a Booking to insert
+    app.post('/booking', async(req, res) =>{
+      const booking = req.body;
+      const query = {treatment: booking.treatment, date: booking.date, patient: booking.patient}
+      const exists = await bookingCollection.findOne(query);
+      if(exists){
+        return res.send({success: false, booking: exists})
+      }
+      const result = await bookingCollection.insertOne(booking);
+      return res.send({success: true, result})
+    })   
+ 
+    // GET Packages API
     app.get('/service', async(req, res) =>{
       const cursor = serviceCollection.find({});
-      const services = await cursor.toArray();
-      res.send(services)
-    })
-    app.post('/service', async(req, res)=>{
-      const doc = {
-        title: "Record of a Shriveled Datum",
-        content: "No bytes, no problem. Just insert a document, in MongoDB",
-      }
-      const result = await packagesCollection.insertOne(doc)
-      res.json(result)
-    });
-  }
-  finally{
+      const service = await cursor.toArray();
+      res.send(service);
+      });
 
+    // GET Booking API
+    app.get('/booking', async(req, res) =>{
+      const cursor = bookingCollection.find({});
+      const booking = await cursor.toArray();
+      res.send(booking);
+      });
+
+  } finally {
+    // await client.close();
   }
 }
 run().catch(console.dir);
