b6c7a6f152166689002bc4d9c0f93076393a2ec0
gm sadiq <gm1sadiq@gmail.com>
2022-11-03 10:48:49 +0600

    initing ssl commerce

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const SSLCommerzPayment = require('sslcommerz')
 const cors = require('cors');
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const jwt = require('jsonwebtoken');
@@ -120,7 +121,6 @@ async function run() {
     // GET Booking API
     app.get('/booking', verifyJWT , async(req, res) =>{
       const patient = req.query.patient;
-      console.log(patient);
       // const author = req.headers.authorization;
       const decodedEmail = req.decoded.email;
 
@@ -149,6 +149,57 @@ async function run() {
     })
     res.send(services)
   })
+
+  // sslcommerz Payment init
+
+app.get('/init', (req, res) => {
+  const data = {
+      total_amount: 100,
+      currency: 'EUR',
+      tran_id: 'REF123',
+      success_url: 'http://localhost:5000/success',
+      fail_url: 'http://yoursite.com/fail',
+      cancel_url: 'http://yoursite.com/cancel',
+      ipn_url: 'http://yoursite.com/ipn',
+      shipping_method: 'Courier',
+      product_name: 'Computer.',
+      product_category: 'Electronic',
+      product_profile: 'general',
+      cus_name: 'Customer Name',
+      cus_email: 'cust@yahoo.com',
+      cus_add1: 'Dhaka',
+      cus_add2: 'Dhaka',
+      cus_city: 'Dhaka',
+      cus_state: 'Dhaka',
+      cus_postcode: '1000',
+      cus_country: 'Bangladesh',
+      cus_phone: '01711111111',
+      cus_fax: '01711111111',
+      ship_name: 'Customer Name',
+      ship_add1: 'Dhaka',
+      ship_add2: 'Dhaka',
+      ship_city: 'Dhaka',
+      ship_state: 'Dhaka',
+      ship_postcode: 1000,
+      ship_country: 'Bangladesh',
+      multi_card_name: 'mastercard',
+      value_a: 'ref001_A',
+      value_b: 'ref002_B',
+      value_c: 'ref003_C',
+      value_d: 'ref004_D'
+  };
+  const sslcommer = new SSLCommerzPayment(process.env.STORE_ID, process.env.STORE_PASS, false) //true for live default false for sandbox
+  sslcommer.init(data).then(data => {
+    res.redirect(data.GatewayPageURL)
+      //process the response that got from sslcommerz 
+      //https://developer.sslcommerz.com/doc/v4/#returned-parameters
+  });
+})
+
+app.post('/success', async(req, res) =>{
+  console.log(req.body);
+  res.status(200).json(req.body)
+})
 
   } finally {
     // await client.close();
