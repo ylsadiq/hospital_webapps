92bef3bd4cf40c864cf103a0a1ac5a2c3ed97a48
gm sadiq <gm1sadiq@gmail.com>
2022-12-19 20:56:57 +0600

    delete object added

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const jwt = require('jsonwebtoken');
 require('dotenv').config()
 const app = express();
@@ -12,21 +12,6 @@ app.use(express.json());
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.jbgbo.mongodb.net/?retryWrites=true&w=majority`
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
-// function verifyJWT(req, res, next) {
-//   const authHeader = req.headers.authorization;
-//   console.log(authHeader);
-//   if(!authHeader){
-//     return res.status(401).send({message: 'unAuthorized access'})
-//   }
-//   const token = authHeader.split(' ')[1];
-//   jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function(err, decoded) {
-//     if(err){
-//       return res.status(403).send({message: "Forbidden access"})
-//     }
-//     req.decoded = decoded;
-//     next() // bar
-//   });
-// }
 function verifyJWT(req, res, next) {
   const authHeader = req.headers.authorization;
   if(!authHeader){
@@ -93,7 +78,7 @@ async function run() {
      // create a Booking to insert
     app.post('/booking', async(req, res) =>{
       const booking = req.body;
-      const query = {treatment: booking?.treatment, date: booking?.date, patient: booking?.patient};
+      const query = {treatment: booking?.treatment, date: booking?.date, patient: booking?.patient, time: booking?.slotTime};
       const exists = await bookingCollection.findOne(query);
       if(exists){
         return res.send({success: false, booking: exists})
@@ -129,7 +114,15 @@ async function run() {
       }else{
         return res.status(403).send({message: 'Forbidden Access'})
       }
-    })
+    });
+
+    // delete Order
+    app.delete("/booking/:id", async (req, res) => {
+    const id = req.params.id;
+    const query = { _id: ObjectId(id) };
+    const result = await bookingCollection.deleteOne(query);
+    res.json(result);
+  });
 
   app.get('/available', async(req, res) =>{
     const date = req.query.date || "Oct 22, 2022";
